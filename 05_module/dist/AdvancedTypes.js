48ed50e3273303321d17e227c3464100637a7285
vovacelidze <louglescom@gmail.com>
2023-01-01 21:52:54 +0200

    added type guards

--- 05_module/dist/AdvancedTypes.js
+++ 05_module/dist/AdvancedTypes.js
@@ -10,7 +10,43 @@ console.log('the same with interface');
 ;
 ;
 console.groupEnd();
-console.group('');
+console.group('Type Guards');
+function combine(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function showFields(e1) {
+    console.log(e1.name);
+    if ('privileges' in e1) {
+        console.log(e1.privileges);
+    }
+    if ('startDate' in e1) {
+        console.log(e1.startDate);
+    }
+}
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo ...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
 console.groupEnd();
 console.group('');
 console.groupEnd();
--- 05_module/src/AdvancedTypes.ts
+++ 05_module/src/AdvancedTypes.ts
@@ -1,3 +1,6 @@
+
+//======================================== Intersaction Types ================================================
+
 console.group('Intersaction Types')
 
 type Admin = {
@@ -34,14 +37,82 @@ interface IEmployee  {
 
 type IElevatedEmployee = Admin & Employee;
 
-//================ If we want to save to interface, short way is below ========================
+//If we want to save to interface, short way is below 
 
 interface MyElevatedEmployee extends Employee, Admin {}
 
 console.groupEnd();
+//======================================== Type Guards ================================================
 
-console.group('')
+console.group('Type Guards')
+
+type ComplexType = string | number;
+
+function combine (a: ComplexType, b: ComplexType) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+// When we have mixed type from objects!
+type GuardsAdmin = {
+  name: string;
+  privileges: string[];
+};
+
+type GuardsEmployee = {
+  name: string;
+  startDate: Date;
+};
+
+type GuardsUnknownObject = GuardsEmployee | GuardsAdmin;
+
+function showFields (e1: GuardsUnknownObject) {
+  console.log(e1.name);
+  if ('privileges' in e1) {
+    console.log(e1.privileges);
+  }
+  if ('startDate' in e1) {
+    console.log(e1.startDate);
+  }
+}
+
+
+// Operator in allow to check if there is a field in object
+// And now if is it a classes
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo ...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+// instanceof - what class does the object belong to
+
 console.groupEnd();
+
 console.group('')
 console.groupEnd();
 console.group('')
