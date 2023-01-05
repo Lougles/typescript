81f683a899566b9d9c4623908d0555ca321973cc
vovacelidze <louglescom@gmail.com>
2023-01-05 14:51:53 +0200

    added practice answers

--- 05_module/dist/Practics.js
+++ 05_module/dist/Practics.js
@@ -29,4 +29,34 @@ class Page extends Component {
         console.log(this.props.title);
     }
 }
+function getPromiseAnswer() {
+    return new Promise((resolve) => {
+        resolve(['Text', 50]);
+    });
+}
+getPromiseAnswer()
+    .then((data) => {
+    console.log(data);
+});
+function compareAnswer(top, bottom) {
+    return {
+        name: top.name,
+        color: top.color,
+        position: bottom.position,
+        weight: bottom.weight,
+    };
+}
+function mergeAnswer(objA, objB) {
+    return Object.assign(objA, objB);
+}
+class ComponentAnswer {
+    constructor(props) {
+        this.props = props;
+    }
+}
+class PageAnswer extends ComponentAnswer {
+    pageInfo() {
+        console.log(this.props.title);
+    }
+}
 //# sourceMappingURL=Practics.js.map
--- 05_module/src/Practics.ts
+++ 05_module/src/Practics.ts
@@ -53,3 +53,60 @@ class Page extends Component {
   }
 }
 
+//======================================== Answers  ================================================
+//Є функція, яка повертає Promise, він повертає масив рядків і чисел, опишіть правильно тип.
+
+function getPromiseAnswer (): Promise<Array<string | number>> {
+  return new Promise((resolve) => {
+    resolve(['Text', 50]);
+  });
+}
+
+getPromiseAnswer ()
+.then((data) => {
+  console.log(data);
+});
+
+//Є функція, вона приймає два аргументи, в один потрапляє name і color, в іншу частину - position і weight. 
+//Потрібно явно вказати, що ці поля з AllType. І сама функція повертає AllType. Скористайтеся Pick.
+
+type AllTypeAnswer = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number
+}
+
+function compareAnswer (top: Pick<AllTypeAnswer, 'name' | 'color'>, bottom: Pick<AllType, 'position' | 'weight'>): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  }
+}
+
+//Вкажіть дженерики для функції об'єднання об'єктів.
+
+
+function mergeAnswer<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+//Тільки додаючи дженерики для класів та інтерфейс, приберіть помилку.
+
+class ComponentAnswer<T> {
+  constructor (public props:T) {
+
+  }
+}
+
+interface IProps {
+  title: string
+}
+
+class PageAnswer extends ComponentAnswer<IProps> {
+  pageInfo () {
+    console.log(this.props.title);
+  }
+}
